9dcedf56d483673800581daf3fb20df141a0cfca
dumbrosio <valeriodambrosio.42@proton.me>
2025-12-12 18:03:59 +0100

    Part Two

--- Day 1 - Secret Entrance/app.js
+++ Day 1 - Secret Entrance/app.js
@@ -5,22 +5,22 @@ const input = fs.readFileSync(filePath, "utf8");
 const lines = input.split("\n");
 
 let zerosCount = 0;
-let rotation = 50;
+let prevPosition = 50;
 
 for (let i = 0; i < lines.length; i++) {
-	if (lines[i].startsWith("R")) {
-		rotation += Number(lines[i].slice(1));
-		const tensAndUnits = rotation % 100;
+	const currentClicks = Number(lines[i].slice(1));
 
-		rotation = tensAndUnits;
-		tensAndUnits === 0 ? zerosCount++ : zerosCount;
+	if (lines[i].startsWith("R")) {
+		const nextPosition = prevPosition + currentClicks;
+
+		zerosCount += Math.floor(nextPosition / 100) - Math.floor(prevPosition / 100);
+		prevPosition = nextPosition;
 	}
 	if (lines[i].startsWith("L")) {
-		rotation -= Number(lines[i].slice(1));
-		const tensAndUnits = rotation % 100;
+		const nextPosition = prevPosition - currentClicks;
 
-		rotation = tensAndUnits;
-		tensAndUnits === 0 ? zerosCount++ : zerosCount;
+		zerosCount += Math.floor((prevPosition - 1) / 100) - Math.floor((nextPosition - 1) / 100);
+		prevPosition = nextPosition;
 	}
 }
 
