92e390fed20e19bb26cd08e7a7e174679e32e50b
dumbrosio <valeriodambrosio.42@proton.me>
2025-12-14 17:16:06 +0100

    Part Two

--- Day 2 - Gift Shop/app.js
+++ Day 2 - Gift Shop/app.js
@@ -4,18 +4,40 @@ const filePath = path.join(__dirname, "input.txt");
 const input = fs.readFileSync(filePath, "utf-8").trim();
 const ranges = input.split(",");
 const invalidIds = [];
+const isPrime = (num) => {
+	for (let i = 2, s = Math.sqrt(num); i <= s; i++) {
+		if (num % i === 0) return false;
+	}
+	return num > 1;
+};
+const areAllDigitsSame = (num) => {
+	const numStr = String(num);
+	const firstDigit = numStr[0];
+
+	return numStr.split("").every((digit) => digit === firstDigit);
+};
 
 for (let i = 0; i < ranges.length; i++) {
 	const range = ranges[i];
 	const [start, end] = range.split("-").map(Number);
 
 	for (let j = start; j <= end; j++) {
-		if (j.toString().length % 2 === 0) {
-			if (
-				j.toString().substring(0, j.toString().length / 2) ===
-				j.toString().substring(j.toString().length / 2)
-			) {
-				invalidIds.push(j);
+		const str = j.toString();
+		const len = str.length;
+
+		if (len > 1 && areAllDigitsSame(j)) {
+			invalidIds.push(j);
+		} else if (!isPrime(len)) {
+			for (let k = 2; k < len; k++) {
+				if (len % k === 0) {
+					const subLen = len / k;
+					const substr = str.substring(0, subLen);
+
+					if (substr.repeat(k) === str) {
+						invalidIds.push(j);
+						break;
+					}
+				}
 			}
 		}
 	}
